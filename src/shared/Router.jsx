fb1c7a33fd0b05a961dfff43b1db1923308b9bad
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-11-08 00:24:33 +0900

    모달창구현으로인한 signup삭제  loginPage이름변경 모달창구현완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Login from "../login/Login";
-
-import Signup from "../login/Signup";
+import Login from "../login/LoginPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
