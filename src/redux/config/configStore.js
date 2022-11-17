21f7203cc90beed838d5c0dd4aa4d8cefacf0898
kkh9960 <kkjh9960@gmail.com>
2022-11-17 14:30:13 +0900

    pull commit

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import authSlice from "../modules/Signup";
-import myInfo from "../modules/MyPageSlice";
-import gameInfo from "../modules/GameSlice";
-
-const store = configureStore({
-  reducer: { authSlice, myInfo, gameInfo },
-  //   middleware: getDefaultMiddleware({
-  //     serializableCheck: false,
-  //   }),
-=======
 import { configureStore } from "@reduxjs/toolkit";
 import BestSlice from "../modules/BsetFiveSlice";
 import MapSlice from "../modules/MapSlice";
@@ -18,9 +6,11 @@ import PostSlice from "../modules/PostSlice";
 
 const store = configureStore({
   reducer: {
-    BestSlice, MapSlice, WeatherSlice, PostSlice
+    BestSlice,
+    MapSlice,
+    WeatherSlice,
+    PostSlice,
   },
->>>>>>> 05d0ff3f4b1fea3a1a04343c3f81d800062f62b7
 });
 
 export default store;
