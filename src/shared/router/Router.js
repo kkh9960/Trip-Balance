29dad93dd051548ec907d9eac611e5dc54dacc5a
seongwoo <seongwoo91@naver.com>
2022-11-21 21:00:36 +0900

    조금수정

--- src/shared/router/Router.js
+++ src/shared/router/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "../../login/LoginPage";
 import MainPage from "../../pages/mainPage/MainPage";
 import MyPage from "../../pages/mypage/MyPage";
 import PostPage from "../../PostPage/PostPage";
@@ -10,6 +9,7 @@ import GameResult from "../../pages/game/GameResult";
 import BoardPostDetail from "../../pages/BoardPostDetail";
 import BoardPostModify from "../../pages/BoardPostModify";
 import Loading from "../../components/Loading/Loading";
+import Signup from "../../login/Signup";
 
 const Router = () => {
   return (
@@ -26,6 +26,7 @@ const Router = () => {
         <Route path="/game/:id" element={<Game />} />
         <Route path="/game/:aa/:id" element={<Game />} />
         <Route path="/gameResult/:aa/:id" element={<GameResult />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
