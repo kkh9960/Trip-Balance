03ec25bfdd92a79242e30d13f360235a25fc47ce
kkh9960 <kkjh9960@gmail.com>
2022-12-08 20:11:04 +0900

    google analytics & amplitude

--- src/components/game/GameStartPage.jsx
+++ src/components/game/GameStartPage.jsx
@@ -15,10 +15,10 @@ export default function GameStartPage() {
       <g.gameStartTextWrap>
         <g.BalanceGameTITLE/>
           <g.gameStartText>
-            자신이 더 선호하는 것을 골라 최종 여행지를 가리는 게임 <br/>
-            몇번의 선택 후 당신에게 맞는 여행지를 추천해 드립니다. <br/>
-            왼쪽 오른쪽 사진 중 느낌이 끌리는 쪽을 선택하세요. <br/>
-            결과보기 화면을 클릭하여 자신이 선호하는 <br/>
+            자신이 더 선호하는 것을 골라 최종 여행지를 가리는 게임
+            몇번의 선택 후 당신에게 맞는 여행지를 추천해 드립니다. 
+            왼쪽 오른쪽 사진 중 느낌이 끌리는 쪽을 선택하세요.
+            결과보기 화면을 클릭하여 자신이 선호하는
             여행지를 확인하세요~
           </g.gameStartText>
 
